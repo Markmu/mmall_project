48ca5aa6c64a60c59eb542703585c0e1095b721c
Mark C. Mu <muchao1303@gmail.com>
2019-02-19 20:59:38 +0800

    add lombok support

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.mmall.common.ServerResponse;
 import com.mmall.service.IFileService;
 import com.mmall.util.FTPUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -14,15 +15,17 @@ import java.io.IOException;
 import java.util.UUID;
 
 @Service("iFileService")
+@Slf4j
 public class FileServiceImpl implements IFileService {
 
-    private static Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+//    private static Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+
 
     public String upload(MultipartFile file, String path) {
         String fileName = file.getOriginalFilename();
         String fileExtensionName = fileName.substring(fileName.lastIndexOf(".") + 1);
         String uploadFileName = new StringBuilder().append(UUID.randomUUID()).append(".").append(fileExtensionName).toString();
-        logger.info("开始上传文件, 文件名: {}, 路径: {}, 新文件名: {}", fileName, path, uploadFileName);
+        log.info("开始上传文件, 文件名: {}, 路径: {}, 新文件名: {}", fileName, path, uploadFileName);
         File fileDir = new File(path);
         if (!fileDir.exists()) {
             fileDir.setWritable(true);
