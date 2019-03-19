db448dc84f339b7180711d14b7f294e4210831eb
Mark C. Mu <muchao1303@gmail.com>
2019-03-19 22:09:38 +0800

    change the method of login to using redis

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -5,12 +5,17 @@ import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import com.mmall.util.Const;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -22,11 +27,12 @@ public class UserController {
 
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
-
+    public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
         ServerResponse<User> sr = iUserService.login(username, password);
         if (sr.isSuccess()) {
-            session.setAttribute(Const.CURRENT_USER, sr.getData());
+            String sessionId = session.getId();
+            CookieUtil.writeLoginToken(httpServletResponse, sessionId);
+            RedisPoolUtil.setex(sessionId, JsonUtil.obj2String(sr.getData()), 60 * 30);
         }
         return sr;
     }
@@ -34,8 +40,8 @@ public class UserController {
 
     @RequestMapping(value = "logout.do", method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<String> logout(HttpSession session) {
-        session.removeAttribute(Const.CURRENT_USER);
+    public ServerResponse<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtil.delLoginToken(request, response);
         return ServerResponse.createBySuccess();
     }
 
@@ -53,8 +59,9 @@ public class UserController {
 
     @RequestMapping(value = "get_user_info.do", method = RequestMethod.GET)
     @ResponseBody
-    public ServerResponse<User> getUserInfo(HttpSession session) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
+    public ServerResponse<User> getUserInfo(HttpServletRequest request) {
+        String sessionId = CookieUtil.readLoginToken(request);
+        User user = JsonUtil.string2Obj(RedisPoolUtil.get(sessionId), User.class);
         if (user == null) {
             return ServerResponse.createByErrorMessage("用户未登录,请重新登录");
         }
--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -1,4 +1,64 @@
 package com.mmall.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
 public class CookieUtil {
+
+    private static final String COOKIE_DOMAIN = ".mmall.com";
+
+    private static final String COOKIE_NAME = "mmall_login_token";
+
+
+    public static void writeLoginToken(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(COOKIE_NAME, token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        //  设置在根目录
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        // 设置cookie有效时间，如果不设置则不会写入硬盘，而是写入内存只对当前页面有效。
+        // -1 代表永久
+        cookie.setMaxAge(60 * 60 * 24 * 365);
+        log.info("write cookie name:{} , value:{}", COOKIE_NAME, token);
+        response.addCookie(cookie);
+    }
+
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+        for (Cookie c : cookies) {
+            log.info("Read cookie name:{} , cookie value:{}", c.getName(), c.getValue());
+            if (StringUtils.equals(COOKIE_NAME, c.getName())) {
+                log.info("Return cookie name:{} , cookie value:{}", c.getName(), c.getValue());
+                return c.getValue();
+            }
+        }
+        return null;
+    }
+
+    public static void delLoginToken(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return;
+        }
+        for (Cookie c : cookies) {
+            log.info("Read cookie name:{} , cookie value:{}", c.getName(), c.getValue());
+            if (StringUtils.equals(COOKIE_NAME, c.getName())) {
+                log.info("Delete cookie name:{} , cookie value:{}", c.getName(), c.getValue());
+                c.setDomain(COOKIE_DOMAIN);
+                c.setPath("/");
+                // 有效期设置为0，浏览其会自动删除该cookie
+                c.setMaxAge(0);
+                response.addCookie(c);
+            }
+        }
+    }
+
 }
